37b83dac1aedacb20f4645a0c8456ffe6caa7f02
joshua-semana <joshuadrummer14@gmail.com>
2024-08-18 03:05:35 +0800

    Remove checking of NDEFReader of a browser

--- components/NFCScanner.tsx
+++ components/NFCScanner.tsx
@@ -13,33 +13,29 @@ const NFCScanner = () => {
   const [scanning, setScanning] = useState(false);
 
   const startScan = async () => {
-    if ("NDEFReader" in window) {
-      const reader = new NDEFReader();
+    const reader = new NDEFReader();
 
-      try {
-        console.log("Scan started...");
-        setScanning(true);
+    try {
+      console.log("Scan started...");
+      setScanning(true);
 
-        await reader.scan();
+      await reader.scan();
 
-        reader.onreading = (event) => {
-          const { message } = event;
+      reader.onreading = (event) => {
+        const { message } = event;
 
-          message.records.forEach((record: NDEFRecord) => {
-            const textDecoder = new TextDecoder(record.encoding);
-            const rfidData = textDecoder.decode(record.data);
+        message.records.forEach((record: NDEFRecord) => {
+          const textDecoder = new TextDecoder(record.encoding);
+          const rfidData = textDecoder.decode(record.data);
 
-            console.log("RFID Detected:", rfidData);
-          });
+          console.log("RFID Detected:", rfidData);
+        });
 
-          setScanning(false);
-        };
-      } catch (error) {
-        console.error("Error during scan:", error);
         setScanning(false);
-      }
-    } else {
-      console.error("Web NFC API is not supported in this browser.");
+      };
+    } catch (error) {
+      console.error("Error during scan:", error);
+      setScanning(false);
     }
   };
 
